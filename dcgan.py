10427d209a2761e54f82cc7ab84d122cd18e2f14
rajathkumarmp <rajathkumarmp1995@gmail.com>
2016-10-09 12:01:09 +0530

    initial

--- dcgan.py
+++ dcgan.py
@@ -20,20 +20,19 @@ from keras.datasets import mnist
 from keras.utils import np_utils
 from keras import initializations
 
-
 def dataInit():
 	'''
 	Load train and noise data
-
 	'''
 	print('Loading the data')
 
 	(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 	X_train = np.concatenate((X_train, X_test), axis=0)
-
+	
 	X_train = (X_train.astype(np.float32) - 127.5)/127.5
+	
 	print('Training Data: ', X_train.shape)
-
+	
 	npRandom = np.random.RandomState(18)
 
 	X_noise = []
@@ -49,8 +48,7 @@ def dataInit():
 
 def saveImage(imageData, imageName, epoch):
 	'''
-	Save Image after every epoch
-
+	Save Image after every 5 epoch
 	'''
 	f, ax = plt.subplots(16, 8)
 	k = 0
@@ -73,23 +71,16 @@ def initNormal(shape, name=None):
 	'''
 	return initializations.normal(shape, scale=0.02, name=name)
 
-
 if __name__ == '__main__':
 
 	batchSize = 128
-
 	nbEpoch = 200
-
 	decayIter = 100
-
 	lr = 0.0002
 
 	X_train, X_noise = dataInit()
-
 	X_train = X_train[:, np.newaxis, :, :]
-
 	numExamples = (X_train.shape)[0]
-
 	numBatches = int(numExamples/float(batchSize))
 
 	print('Number of examples: ', numExamples)
@@ -103,36 +94,26 @@ if __name__ == '__main__':
 	generator = Sequential()
 	generator.add(Dense( input_dim=100, output_dim=(128*7*7), init=initNormal))
 	generator.add(Activation('relu'))
-	generator.add(Reshape((128, 7, 7)))
-	
+	generator.add(Reshape((128, 7, 7)))	
 	generator.add(UpSampling2D(size=(2, 2)))
 	generator.add(Convolution2D(64, 5, 5, border_mode='same'))
-	generator.add(Activation('relu'))
-		
+	generator.add(Activation('relu'))		
 	generator.add(UpSampling2D(size=(2, 2)))
 	generator.add(Convolution2D(1, 5, 5, border_mode='same'))
 	generator.add(Activation('tanh'))
-
 	generator.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
-
 
 	print('Discriminator Model')
 
 	discriminator = Sequential()
-	
 	discriminator.add(Convolution2D(64, 5, 5, border_mode='same', subsample=(2,2), input_shape=(1,28,28), init=initNormal))
 	discriminator.add(LeakyReLU(0.2))
-
 	discriminator.add(Convolution2D(128, 5, 5, border_mode='same', subsample=(2,2)))
 	discriminator.add(LeakyReLU(0.2))
-
 	discriminator.add(Flatten())
-
 	discriminator.add(Dense(1))
 	discriminator.add(Activation('sigmoid'))
-
 	discriminator.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
-
 
 	discriminator.trainable = False
 
@@ -146,14 +127,12 @@ if __name__ == '__main__':
 
 	discriminator.trainable = True
 
-
 	if not os.path.exists('images'):
-    		os.makedirs('images')
-    if not os.path.exists('models'):
-    		os.makedirs('models')
-    if not os.path.exists('metrics'):
-    		os.makedirs('metrics')
-
+		os.makedirs('images')
+	if not os.path.exists('models'):
+		os.makedirs('models')
+	if not os.path.exists('metrics'):
+		os.makedirs('metrics')
 
 	dLoss = []
 	gLoss = []
@@ -165,48 +144,37 @@ if __name__ == '__main__':
 		for i in range(numBatches):
 
 			noisePredictBatch = X_noise[np.random.randint(numExamples, size = batchSize)]
-
 			noiseDataBatch = generator.predict(noisePredictBatch)
-
 			origDataBatch = X_train[np.random.randint(numExamples, size = batchSize)]
-
 			noiseLabelsBatch, origLabelsBatch = np.zeros(batchSize).astype(int), np.ones(batchSize).astype(int)
-
 			trainBatch = np.concatenate((noiseDataBatch, origDataBatch), axis = 0)
-
 			trainLabels = np.concatenate((noiseLabelsBatch, origLabelsBatch))
-
 			trainBatch, trainLabels = shuffle(trainBatch, trainLabels)
-
 			discriminatorLoss = discriminator.train_on_batch(trainBatch, trainLabels)
 
-			dcganLabels = np.ones(batchSize).astype(int)
-			
+			dcganLabels = np.ones(batchSize).astype(int)			
 			discriminator.trainable = False
-
 			dcganLoss = dcgan.train_on_batch(noisePredictBatch, dcganLabels)
-
 			discriminator.trainable = True
 
 		dLoss.append(discriminatorLoss)
 		gLoss.append(dcganLoss)
 		
 		if (epoch % 5 == 0) or (epoch == 1):
+
 			saveImage(noiseDataBatch, 'generated', epoch)
 			print('after epoch: ', epoch)
 			print ('dcgan Loss: ', dcganLoss, '\t discriminator loss', discriminatorLoss)
-			
 			generator.save('models/generator_'+str(epoch)+'.h5')
 
 		if epoch > decayIter :
+
 			lrD = discriminator.optimizer.lr.get_value()
 			lrG = generator.optimizer.lr.get_value()
 			discriminator.optimizer.lr.set_value((lrD - lr/decayIter).astype(np.float32))
 			generator.optimizer.lr.set_value((lrG - lr/decayIter).astype(np.float32))
 			print('learning rate linearly decayed')
 
-	
 	np.save('metrics/dLoss.npy', np.array(dLoss))
 	np.save('metrics/gLoss.npy', np.array(gLoss))
 	print('Peace')
-
